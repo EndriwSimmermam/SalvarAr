c23df78bb0d2fffc593d125999448aae4a0765bb
EndriwSimmermam <113243731+EndriwSimmermam@users.noreply.github.com>
2023-08-04 06:08:56 -0300

    Update AutoSendAR.py

--- AutoSendAR.py
+++ AutoSendAR.py
@@ -42,40 +42,43 @@ button.pack(padx=40, pady=20)
 janela.bind('<Return>', enviardados)
 janela.mainloop()
 
+strarnomedoc = (f'{ar} - {doc}.pdf')
+
 # entrando no navegador
 driver = webdriver.Edge()
 
 sleep(2)
 
-driver.get(
-    'http://vcremona/Ripplestone/SignOn.aspx?ReturnUrl=%2fripplestone%2fHome.aspx')
+driver.get('http://vcremona/Ripplestone/SignOn.aspx?ReturnUrl=%2fripplestone%2fHome.aspx')
 
 # entrando com usuario
-usuario = webdriver.find_element(By.XPATH, 'txtUserID')
+usuario = driver.find_element(By.XPATH, '/html/body/form/div[3]/div[2]/div[2]/table/tbody/tr[1]/td/table/tbody/tr[1]/td[2]/input')
 usuario.send_keys('endriw')
 
-senha = webdriver.find_element(By.XPATH, 'txtPassword')
+senha = driver.find_element(By.XPATH, '/html/body/form/div[3]/div[2]/div[2]/table/tbody/tr[1]/td/table/tbody/tr[2]/td[2]/input')
 senha.send_keys('123')
 
-button_login = webdriver.find_element(By.XPATH, 'btnLogin').click()
+button_login = driver.find_element(By.XPATH, '/html/body/form/div[3]/div[2]/div[2]/table/tbody/tr[1]/td/table/tbody/tr[3]/td/input').click()
 
 # abrindo caixa de pesquisa ar
-ar_link = webdriver.find_element(
-    By.XPATH, 'ctl00_MainContent_ASPxPageControl1_panelMyDocuments_repeaterFavorites_ctl01_lnkbtnRunFav').click()
+ar_link = driver.find_element(By.XPATH, '/html/body/form/div[3]/div[3]/div[4]/div/div/div/div[2]/div[2]/table/tbody/tr[2]/td/div/div/table/tbody/tr[1]/td[2]/a').click()
 
-ar_box = webdriver.find_element(By.XPATH, 'pePromptTextBox').click()
+ar_box = driver.find_element(By.XPATH, '/html/body/table/tbody/tr/td/table/tbody/tr[2]/td/div/form/div/div/fieldset/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td[1]/table/tbody/tr/td/table/tbody/tr[2]/td[1]/input').click()
 ar_box.send_keys(f'{ar}')
 
-btn_ok = webdriver.find_element(By.XPATH, 'pePromptButton').click()
+btn_ok = driver.find_element(By.XPATH, '/html/body/table[1]/tbody/tr/td/table/tbody/tr[2]/td/div/form/div/div/table/tbody/tr/td/table/tbody/tr/td[2]/div/a').click()
 
-sleep(60)
+sleep(30)
 
 # exportando o ar
-ar_export = webdriver.find_element(
-    By.XPATH, 'IconImg_CrystalReportViewer1_toptoolbar_export').click()
-ar_export2 = webdriver.find_element(By.XPATH, 'wizbutton').click()
+ar_export = driver.find_element(By.XPATH, '/html/body/form/div[3]/div[3]/div[4]/div/div/div/div/div[4]/div/div[1]/table/tbody/tr/td[1]/table/tbody/tr/td[1]/table/tbody/tr/td[2]/table/tbody/tr/td[1]/table/tbody/tr/td[5]/table/tbody/tr/td/div/img').click()
+ar_export2 = driver.find_element(By.XPATH, '/html/body/table[3]/tbody/tr/td/table/tbody/tr[2]/td/table/tbody/tr[2]/td/table/tbody/tr/td/table/tbody/tr/td[2]/nobr/a').click()
 
 sleep(2)
 
 # fechar o navegador
 driver.quit()
+
+os.rename('C:\Users\endriw\Downloads\CrystalReportViewer1.pdf', f'R:\Almoxarifado\Arquivos Comuns\AA - ARs de Compra Direta\ARs de 2023\{strarnomedoc}')
+
+os.startfile("outlook")
